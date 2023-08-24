7d1b3396023a5f27058c87e5986bca27dc332c67
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-08-24 11:39:15 -0600

    My first C++ program hello world

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Jeremy Bergen
+
+This is a hello world program!!!
+*/
+
+//This is to print out
+#include <iostream>
+
+//This is to use the standard namespace
+using namespace std;
+
+//This is the first function all C/C++ programs have to have
+int main() {
+    cout << "Hello World" << endl; //This prints out to the console
+    
+    return 0; //I'll talk more about you later.......suspicious.....
+}
