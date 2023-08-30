af2a02490c1d2d376a16f04002b3e36b01a24554
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-08-30 11:50:51 -0600

    wednesday 0830

---
+++ lectures/datatypes.cpp
@@ -0,0 +1,49 @@
+/*
+Name: Jeremy Bergen
+
+All about the data...types...
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int number1;
+
+    number1 = 2147483646;
+
+    cout << "number1: " << number1 << endl;
+
+    number1 = number1 + 1;
+
+    cout << "number1: " << number1 << endl;
+
+    number1++;
+
+    cout << "number1: " << number1 << endl;
+
+    number1++;
+
+    cout << "number1: " << number1 << endl;
+    // char someChar;
+    // int someInt = 42;
+    // float thisIsAFloat;
+    
+    // someChar = 'J';
+    // someInt = 42;
+    // thisIsAFloat = 42.95;
+
+    // cout << "someInt: " << someInt << endl;
+    // cout << "someChar: " << someChar << endl;
+    // cout << "thisIsAFloat: " << thisIsAFloat << endl << endl;
+
+    // someChar = 'B';
+    // someInt = -6000;
+    // thisIsAFloat = -324832.3;
+
+    // cout << "someInt: " << someInt << endl;
+    // cout << "someChar: " << someChar << endl;
+    // cout << "thisIsAFloat: " << thisIsAFloat << endl << endl;
+
+    return 0;
+}
