4b751876962a9382ef4a42de8cbb03c43f6cd9ae
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-09-25 19:44:37 -0600

    tuesday 0926

---
+++ lectures/loops.cpp
@@ -0,0 +1,72 @@
+/*
+Name: Jeremy Bergen
+
+Looping
+*/
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+    for(int i = 0; i < 1; i++) {
+        for(int j = 0; j < 3; j++) {
+            for(int k = 0; k < 10; k++) {
+                cout << "i, j: " << i << ", " << j << endl;
+            }
+        }
+        // cout << i << ": Hello World" << endl;
+    }
+    // cout << "i: " << i << endl;
+    // for(int i = 0; i < 10; i++) {
+    //     // if (i%2 == 0) {
+    //     //     continue;
+    //     // }
+    //     if (floor(log2(i)) != log2(i)) {
+    //         continue;
+    //     }
+    //     cout << "log2(i): " << log2(i) << endl;
+    // }
+    // int num1 = -1;
+
+    // cout << boolalpha << "num1 > 0 && num1 <= 20: " << (num1 > 0 && num1 <= 20) << endl;
+    // for( ; !(num1 > 0 && num1 <= 20); ) {
+    //     cout << "Please enter a number between 1 and 20: " << endl;
+    //     cin >> num1;
+    //     if (!(num1 > 0 && num1 <= 20)) {
+    //         cout << "Why can't you follow the rules...I'm sad now... Enter a value between 1 and 20." << endl;
+    //     }
+    // }
+
+    cout << "Outside for loop" << endl;
+
+    // int j = 0;
+    // for(unsigned int i = 4200000000; j > -1; i += 1000) {
+    //     cout << i << ": Hello World" << endl;
+    // }
+
+    return 0;
+}
+
+
+
+
+
+    // int num1;
+    // int num2;
+
+    // num1 = 42;
+    // num2 = num1;
+
+    // //num1 += 1;
+    // //num1 = num1 + 1;
+    // num1++;
+    // num1--;
+    // ++num1;
+    // --num1;
+
+    // cout << "num2: " << num2 << endl;
