ed2b8ce9ccf561d133dae70b5ad2610deb2a03e5
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-10-10 11:49:19 -0600

    tuesday 1010

--- lectures/arrays.cpp
+++ lectures/arrays.cpp
@@ -8,53 +8,125 @@ Arrays!!!!
 
 using namespace std;
 
+size_t getArrSize();
+void getNums(size_t, int[]);
+void printNums(int*, size_t);
+void copyArray(int[], int[], size_t);
+
 int main(int argc, char *argv[]) {
-    unsigned int arrSize = 100;
-    int numbers[] = {42, 15, 23, 9000, 1234, -1};
-    unsigned int tmpNum;
+    size_t arrSize = getArrSize();
+    int *ptr;
+    // unsigned long arrSize = 10;
 
-    int* ptr;
-    // unsigned int counter = 0;
+    int numbers[arrSize];
+    int numbers2[arrSize];
+    int *numbers3;
 
-    // for(unsigned int i = 0; i < arrSize; i++) {
-    //     numbers[i] = -1;
-    // }
-
-    // cout << "Enter whole positive numbers or -1 to quit" << endl;
-    // for(unsigned int i = 0; i < arrSize; i++) {
-        
-    //     cout << "What is the number? ";
-    //     cin >> tmpNum;
-    //     if(tmpNum == -1) {
-    //         numbers[i] = -1;
-    //         break;
-    //     }
-    //     numbers[i] = tmpNum;
-    //     // counter++;
-    //     // cin >> numbers[i];
-    // }
-
-    // for(unsigned int i = 0; i < arrSize; i++) {
-    // int i = 0;
-    // while(numbers[i] != -1) {
-    
-    // for(unsigned int i = 0; i < arrSize; i++) {
-    //     if(numbers[i] == -1) {
-    //         break;
-    //     }
-    //     cout << "numbers[" << i << "]: " << numbers[i] << endl;
-    //     // i++;
-    // }
-    cout << "&numbers[0]: \t" << &numbers[0] << endl;
-    cout << "&numbers: \t" << numbers << endl;
     ptr = numbers;
-    // ptr = &numbers;
-    // ptr = &numbers[0];
 
-    for(int i = 0; i < sizeof(numbers)/sizeof(int); i++) {
-        cout << "*ptr: " << *(ptr) << endl;
-        ptr++;
+    getNums(arrSize, ptr);
+
+    cout << "numbers: " << endl;
+    printNums(numbers, arrSize);
+
+    copyArray(numbers, numbers2, arrSize);
+
+    numbers[0] = 8;
+
+    cout << "numbers2 after copy: " << endl;
+    printNums(numbers2, arrSize);
+
+    numbers3 = numbers;
+
+    cout << "numbers3: " << endl;
+    printNums(numbers3, arrSize);
+
+    return 0;
+}
+
+void copyArray(int source[], int destination[], size_t arrSize) {
+    for(size_t i = 0; i < arrSize; i++) {
+        destination[i] = source[i];
     }
+}
+
+void printNums(int* numbers, size_t arrSize) {
+    // size_t arrSize = sizeof(numbers)/sizeof(int); WILL NOT WORK!!!
+    for(size_t i = 0; i < arrSize; i++) {
+        cout << "numbers[" << i << "]: " << numbers[i] << endl;
+    }
+}
+
+void getNums(size_t arrSize, int numbers[]) {
+    cout << "Please enter " << arrSize << " numbers." << endl;
+    for(size_t i = 0; i < arrSize; i++) {
+        cin >> numbers[i];
+    }
+}
+
+size_t getArrSize() {
+    size_t arrSize;
+    cout << "How many numbers do you want? ";
+    cin >> arrSize;
+    return arrSize;
+}
+
+
+
+
+
+
+
+
+
+    // unsigned int arrSize = 100;
+    // int numbers[] = {42, 15, 23, 9000, 1234, -1};
+    // // int number[10];
+    // unsigned int tmpNum;
+
+    // int* ptr;
+    // // unsigned int counter = 0;
+
+    // // for(unsigned int i = 0; i < arrSize; i++) {
+    // //     numbers[i] = -1;
+    // // }
+
+    // // cout << "Enter whole positive numbers or -1 to quit" << endl;
+    // // for(unsigned int i = 0; i < arrSize; i++) {
+        
+    // //     cout << "What is the number? ";
+    // //     cin >> tmpNum;
+    // //     if(tmpNum == -1) {
+    // //         numbers[i] = -1;
+    // //         break;
+    // //     }
+    // //     numbers[i] = tmpNum;
+    // //     // counter++;
+    // //     // cin >> numbers[i];
+    // // }
+
+    // // for(unsigned int i = 0; i < arrSize; i++) {
+    // // int i = 0;
+    // // while(numbers[i] != -1) {
+    
+    // // for(unsigned int i = 0; i < arrSize; i++) {
+    // //     if(numbers[i] == -1) {
+    // //         break;
+    // //     }
+    // //     cout << "numbers[" << i << "]: " << numbers[i] << endl;
+    // //     // i++;
+    // // }
+    // cout << "&numbers[0]: \t" << &numbers[0] << endl;
+    // cout << "&numbers: \t" << numbers << endl;
+    // ptr = numbers;
+    // // ptr = &numbers;
+    // // ptr = &numbers[0];
+
+    // for(int i = 0; i < sizeof(numbers)/sizeof(int); i++) {
+    //     cout << *(ptr+i) << " == " << numbers[i] << endl;
+    //     // cout << "*ptr: " << *(ptr) << endl;
+    //     // ptr++;
+    // }
 
     // cout << "sizeof(string): " << sizeof(string) << endl;
     // cout << "sizeof(numbers): " << sizeof(numbers) << endl;
@@ -77,5 +149,3 @@ int main(int argc, char *argv[]) {
     // cout << "&numbers[3]: " << &numbers[3] << endl;
     // cout << "&numbers[4]: " << &numbers[4] << endl;
 
-    return 0;
-}
