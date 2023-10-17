2ab12892dd912b6b3f12fd0947efcefbbf33e863
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-10-16 20:52:49 -0600

    tuesday 1017

--- lectures/strings.cpp
+++ lectures/strings.cpp
@@ -4,16 +4,101 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    string fName;
-    string lName;
-    string fullName;
+    // size_t foxIdx;
+    // string newStr;
+    
+    string phrase = "The quick brown fox jumps over the lazy dog.";
+    string words[phrase.length()];
 
-    fName = "Jeremy";
-    lName = "Bergen";
+    string tmpWord = "";
+    size_t counter = 0;
 
-    fullName = fName + " " + lName;
+    // First iteration:
+    // for(auto it = phrase.begin(); it != phrase.end(); it++) {
+    //     if(*it == ' ') {
+    //         words[counter] = tmpWord;
+    //         tmpWord.clear();
+    //         counter++;
+    //         continue;
+    //     }
+    //     tmpWord.push_back(*it);
+    // }
+    // words[counter] = tmpWord;
+    // tmpWord.clear();
+    // counter++;
+    
 
-    cout << "Hello " << fullName << endl;
+    // Second iteration:
+    for(size_t i = 0; i < phrase.length(); i++) {
+        size_t foundIdx = phrase.find(" ", i);
+        // cout << "DEBUG: foundIdx: " << foundIdx << endl;
+        if(foundIdx == string::npos) {
+            tmpWord = phrase.substr(i);
+            words[counter] = tmpWord;
+            counter++;
+            break;
+        }
+        tmpWord = phrase.substr(i, foundIdx-i);
+        words[counter] = tmpWord;
+        counter++;
+        // cout << "DEBUG: counter: " << counter << endl;
+        tmpWord.clear();
+        i = foundIdx;
+    }
+
+    for(size_t i = 0; i < counter; i++) {
+        cout << "DEBUG: words[" << i << "]: " << words[i] << endl;
+    }
+
+    // phrase.clear();
+    // phrase = "";
+
+    // cout << "phrase: " << phrase << endl;
+
+    // phrase.append("On the end");
+    // phrase.push_back('!');
+    // phrase.insert(17, 10, '!');
+
+    // cout << "phrase: " << phrase << endl;
+
+    // cout << "phrase.find(\"fox\"): " << phrase.find("fox") << endl;
+    // foxIdx = phrase.find("fox");
+
+    // newStr = phrase.substr(foxIdx, 3);
+
+    // cout << "newStr: " << newStr << endl;
+
+    // cout << "phrase.empty(): " << boolalpha << phrase.empty() << endl;
+    // cout << "phrase == \"\": " << boolalpha << (phrase == "") << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // string fName;
+    // string lName;
+    // string fullName;
+
+    // fName = "Jeremy";
+    // lName = "Bergen";
+
+    // fullName = fName + " " + lName;
+
+    // cout << "Hello " << fullName << endl;
 
     // fullName[8] = '\0';
     // cout << "DEBUG: fullName: " << fullName.c_str() << endl;
@@ -40,11 +125,9 @@ int main(int argc, char *argv[]) {
     // cout << (char)(fullName.front()+10) << endl;
     // cout << *(fullName.begin()+1) << endl;
 
-    for(char it : fullName) {
-        cout << it << endl;
-    }
+    // for(char it : fullName) {
+    //     cout << it << endl;
+    // }
 
     // cout << "The first character in fullName is: " << fullName[0] << endl;
     // cout << "The size of fullName[0]: " << sizeof(fullName[0]) << endl;
-    return 0;
-}
